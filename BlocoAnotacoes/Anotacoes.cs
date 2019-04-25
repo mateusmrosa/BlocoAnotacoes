e395b93fe868a25b9c5546040940e02a74c51cdc
Mateus <mateusrttim@hotmail.com>
2019-04-24 22:09:33 -0300

    implementando anotaçoes

--- BlocoAnotacoes/Anotacoes.cs
+++ BlocoAnotacoes/Anotacoes.cs
@@ -8,5 +8,47 @@ namespace BlocoAnotacoes
 {
     class Anotacoes
     {
+        private string titulo;
+        private string texto;
+        private DateTime data;
+
+        public Anotacoes(string titulo, string texto, DateTime data)
+        {
+            this.titulo = titulo;
+            this.texto = texto;
+            this.data = data;
+        }
+
+        public Anotacoes()
+        {
+            titulo = "";
+            texto = "";
+            data = Convert.ToDateTime("");
+        }
+
+        public void setTitulo(string valor)
+        {
+            this.titulo = valor;
+        }
+        public string getTitulo()
+        {
+            return this.titulo;
+        }
+        public void setTexto(string valor)
+        {
+            this.texto = valor;
+        }
+        public string getTexto()
+        {
+            return this.texto;
+        }
+        public void setData(DateTime valor)
+        {
+            this.data = valor;
+        }
+        public DateTime getData()
+        {
+            return this.data;
+        }
     }
 }
