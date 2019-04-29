15593164ad1fa91d8b905454ed27505c6aaf0bf2
Mateus <mateusrttim@hotmail.com>
2019-04-29 19:38:02 -0300

    implementando BlocoAnotacoes

---
+++ BlocoAnotacoes/Anotacao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocoAnotacoes
+{
+    class Anotacao
+    {
+        private string titulo;
+        private string texto;
+        private DateTime data;
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Texto { get => texto; set => texto = value; }
+        public DateTime Data { get => data; set => data = value; }
+
+        public Anotacao(string titulo, string texto, DateTime data)
+        {
+            this.Titulo = titulo;
+            this.Texto = texto;
+            this.Data = data;
+        }
+
+        public Anotacao()
+        {
+            Titulo = "";
+            Texto = "";
+            Data = Convert.ToDateTime("");
+        }
+
+        //public void setTitulo(string valor)
+        //{
+        //    this.titulo = valor;
+        //}
+        //public string getTitulo()
+        //{
+        //    return this.titulo;
+        //}
+        //public void setTexto(string valor)
+        //{
+        //    this.texto = valor;
+        //}
+        //public string getTexto()
+        //{
+        //    return this.texto;
+        //}
+        //public void setData(DateTime valor)
+        //{
+        //    this.data = valor;
+        //}
+        //public DateTime getData()
+        //{
+        //    return this.data;
+        //}
+    }
+}
--- BlocoAnotacoes/BlocoAnotacoes.cs
+++ BlocoAnotacoes/BlocoAnotacoes.cs
@@ -8,6 +8,91 @@ namespace BlocoAnotacoes
 {
     class BlocoAnotacoes
     {
+        class BlocoAnotacao
+        {
+            private string _nome;
+            private int _numPaginas;
+            private List<Anotacao> _Anotacoes;
 
+            public BlocoAnotacao(string nome)
+            {
+                _nome = nome;
+                _numPaginas = 0;
+                _Anotacoes = new List<Anotacao>();
+            }
+            public BlocoAnotacao()
+            {
+                _nome = "";
+                _numPaginas = 0;
+                _Anotacoes = new List<Anotacao>();
+            }
+
+            public BlocoAnotacao(string nome, int numPaginas, List<Anotacao> blocoAnotacoes)
+            {
+                _nome = nome;
+                _numPaginas = numPaginas;
+                _Anotacoes = blocoAnotacoes;
+            }
+
+            public void Inserir(string titulo, string texto, DateTime data)
+            {
+                Anotacao anotacao = new Anotacao( titulo, texto, data);
+                Anotacoes.Add(anotacao);
+                _numPaginas += 1;
+            }
+
+            public void Inserir(Anotacao anotacao)
+            {
+                Anotacoes.Add(anotacao);
+                _numPaginas += 1;
+            }
+
+            public Anotacao BuscaAnotacao(string titulo)
+            {
+                return _Anotacoes.Find(x => x.Titulo.Contains(titulo));
+            }
+
+            public Boolean BuscaAnotacoes(Anotacao anotacao)
+            {
+                return _Anotacoes.Contains(anotacao);
+            }
+
+            public List<Anotacao> BuscaAnotacoes(string filtro)
+            {
+                List<Anotacao> ListaAux= null;
+                foreach (Anotacao nota in _Anotacoes)
+                {
+                    if (nota.Texto == filtro || nota.Titulo == filtro)
+                    {
+                        if (ListaAux.Count == 0)
+                            ListaAux = new List<Anotacao>();
+
+                    }
+                }
+                return ListaAux;
+            }
+
+            //public Anotacao GetAnotacao(int index)
+            //{
+            //    Anotacao anot = new Anotacao();
+            //    for (int i = 0; i <= _Anotacoes.Count - 1; i++)
+            //    {
+            //        if (_Anotacoes[i].Index == index)
+            //        {
+            //            anot = _Anotacoes[i];
+            //            break;
+            //        }
+            //    }
+            //    return anot;
+            //}
+
+            public string Nome { get => _nome; set => _nome = value; }
+            public int NumPaginas { get => _numPaginas; set => _numPaginas = value; }
+            public List<Anotacao> Anotacoes { get => _Anotacoes; set => _Anotacoes = value; }
+
+
+
+
+        }
     }
 }
